63683bc28239c8ebc74ea6297515dc6ad738f54d
Roh Tae Gyu <162882540+tiagyu@users.noreply.github.com>
2024-05-20 16:00:08 +0900

    Add files via upload

---
+++ 알고리즘 코딩테스트_python/01 자료구조/002.py
@@ -0,0 +1,19 @@
+#평균 구하기
+n = int(input())
+score = list(map(int,input().split()))
+score.sort()
+max = score[-1]
+
+sum = 0
+for i in score:
+    average = (i / max) * 100
+    sum += average/n
+
+print(sum)
+
+#코드 구현(책)
+n = input()
+mylist = list(map(int,input().split()))
+mymax = max(mylist)
+sum = sum(mylist)
+print(sum*100/mymax/int(n)) #한 과목과 관련된 수식을 총합한 후 관련 수식으로 변환해 로직을 간단하게 할 수 있음
